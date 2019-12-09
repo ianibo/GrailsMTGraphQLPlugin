64e1150544ae5b67460ff955f8b72bafbbcc9018
Ian Ibbotson <ianibbo@googlemail.com>
2019-12-09 17:27:05 +0000

    Different implementation of schema factory

--- graphqlmt/src/main/groovy/graphqlmt/GraphqlSchemaFactory.groovy
+++ graphqlmt/src/main/groovy/graphqlmt/GraphqlSchemaFactory.groovy
@@ -1,14 +1,21 @@
 package graphqlmt
 
 import grails.core.GrailsClass
-import graphql.schema.idl.SchemaGenerator;
-import graphql.schema.idl.TypeDefinitionRegistry;
-import graphql.schema.GraphQLSchema;
 import grails.core.GrailsApplication
 import grails.core.support.GrailsApplicationAware
 import groovy.util.logging.Slf4j
 import graphql.language.ObjectTypeDefinition;
 import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.TypeDefinitionRegistry;
+
+import static graphql.schema.GraphQLArgument.newArgument
+import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition
+import static graphql.schema.GraphQLList.list
+import static graphql.schema.GraphQLObjectType.newObject
+
 
 @Slf4j
 class GraphqlSchemaFactory implements GrailsApplicationAware {
@@ -22,20 +29,36 @@ class GraphqlSchemaFactory implements GrailsApplicationAware {
   GraphQLSchema generate() {
     GraphQLSchema result = null;
 
-    RuntimeWiring.Builder wiring_builder = RuntimeWiring.newRuntimeWiring()
+    // RuntimeWiring.Builder wiring_builder = RuntimeWiring.newRuntimeWiring()
+
+    GraphQLObjectType.Builder queryType = newObject().name('Query')
+    GraphQLObjectType.Builder mutationType = newObject().name('Mutation')
+
 
     (grailsApplication.getArtefacts("Domain")).each {GrailsClass dc ->
       log.debug("registerDomainClass(${dc}) - ${dc.getName()}");
       // grailsApplication.mainContext.graphQLService.registerDomainClass(gc);
-      def object_type_definition_builder = ObjectTypeDefinition.newObjectTypeDefinition()
-      object_type_definition_builder.name(dc.getName())
-      typeRegistry.add(object_type_definition_builder.build());
+      // def object_type_definition_builder = ObjectTypeDefinition.newObjectTypeDefinition()
+      // object_type_definition_builder.name(dc.getName())
+      // typeRegistry.add(object_type_definition_builder.build());
+
+      //GraphQLFieldDefinition.Builder queryOne = newFieldDefinition()
+      //                           .name("get${dc.getName()}".toString())
+      //                           .type(objectType)
+      //                           .description(getOperation.description)
+      //                           .dataFetcher(new InterceptingDataFetcher(entity, serviceManager, queryInterceptorInvoker, GET, getFetcher))
+
     }
 
 
     // https://github.com/graphql-java/graphql-java/blob/master/src/main/java/graphql/schema/idl/RuntimeWiring.java
+    result = GraphQLSchema.newSchema()
+               .query(queryType)
+               .mutation(mutationType)
+               .build() // additionalTypes)
 
-    result = schemaGenerator.makeExecutableSchema(typeRegistry, wiring_builder.build());
+
+    // result = schemaGenerator.makeExecutableSchema(typeRegistry, wiring_builder.build());
 
     return result;
   }
