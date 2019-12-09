6ec40d204c5b9e6e99dec4eeac143b504d7ebede
Ian Ibbotson <ianibbo@googlemail.com>
2019-12-09 17:49:21 +0000

    working on controller

--- graphqlmt/grails-app/controllers/graphqlmt/GraphqlController.groovy
+++ graphqlmt/grails-app/controllers/graphqlmt/GraphqlController.groovy
@@ -8,15 +8,41 @@ import graphql.ExecutionResult
 import graphql.GraphQL
 import groovy.transform.CompileStatic
 import org.springframework.context.MessageSource
+import graphql.GraphQL
+import groovy.transform.CompileStatic
 
-@CompileStatic
+
 class GraphqlController {
 
   static responseFormats = ['json', 'xml']
+  GraphQL graphQL
 
   // https://github.com/grails/gorm-graphql/blob/master/plugin/grails-app/controllers/org/grails/gorm/graphql/plugin/GraphqlController.groovy
   def index() {
-     Map<String, Object> result = new LinkedHashMap<>()
-     result
+
+    if (request.contentLength != 0 && request.method != 'GET' ) {
+      String encoding = request.characterEncoding ?: 'UTF-8'
+      InputStream is = request.getInputStream();
+      String body = IOUtils.toString(is, encoding)
+    } else {
+      // graphQLRequest = GraphQLRequestUtils.graphQLRequestWithParams(params)
+    }
+
+    Map<String, Object> result = new LinkedHashMap<>()
+
+    // ExecutionResult executionResult = graphQL.execute(ExecutionInput.newExecutionInput()
+    //             .query(graphQLRequest.query)
+    //             .operationName(graphQLRequest.operationName)
+    //             //.context(context)
+    //             .root(context) // This we are doing do be backwards compatible
+    //             .variables(graphQLRequest.variables)
+    //             .build())
+
+    // if (executionResult.errors.size() > 0) {
+    //   result.put('errors', executionResult.errors)
+    // }
+    // result.put('data', executionResult.data)
+
+    result
   }
 }
