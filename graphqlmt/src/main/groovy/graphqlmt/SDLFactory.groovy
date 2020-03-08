05b4758655c6b815fb4461de411f41789f2859f2
Ian Ibbotson <ianibbo@gmail.com>
2020-03-08 11:55:19 +0000

    defensive code around stativ graphql block

--- graphqlmt/src/main/groovy/graphqlmt/SDLFactory.groovy
+++ graphqlmt/src/main/groovy/graphqlmt/SDLFactory.groovy
@@ -92,10 +92,15 @@ type DeleteResult {
 
       Map graphql_config = grails.util.GrailsClassUtils.getStaticPropertyValue(value.getJavaClass(), 'graphql') as Map
       if ( graphql_config != null ) {
-        log.debug("Class has static graphql config... process");
-        graphql_config.queries.each { k,v ->
-          log.debug("Adding ${k}(luceneQueryString: String) : ${key}PagedResult\n");
-          sw.write("  ${k}(luceneQueryString: String) : ${key}PagedResult\n");
+        if ( graphql_config instanceof Map ) {
+          log.debug("Class has static graphql config... process");
+          graphql_config.queries.each { k,v ->
+            log.debug("Adding ${k}(luceneQueryString: String) : ${key}PagedResult\n");
+            sw.write("  ${k}(luceneQueryString: String) : ${key}PagedResult\n");
+          }
+        }
+        else {
+          log.warn("${value.getJavaClass()} has graphql member but it is an instance of ${graphql_config.class.name} with value of ${graphql_config}. Expected map");
         }
       }
     }
